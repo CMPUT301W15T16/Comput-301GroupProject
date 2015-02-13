a2f5c0ff0bf38ac6997a7a3afc284ec977dc662b
leonsariel <leonsariel@gmail.com>
2015-02-13 04:40:57 -0700

    add test case 7 and create methods for classes

--- Team16APP/gen/com/example/team16app/R.java
+++ Team16APP/gen/com/example/team16app/R.java
@@ -30,16 +30,18 @@ public final class R {
     public static final class layout {
         public static final int activity_add_claim=0x7f030000;
         public static final int activity_add_item=0x7f030001;
-        public static final int activity_approver_claim_view=0x7f030002;
-        public static final int activity_main=0x7f030003;
-        public static final int activity_take_photo=0x7f030004;
+        public static final int activity_approver_claim_list=0x7f030002;
+        public static final int activity_approver_claim_view=0x7f030003;
+        public static final int activity_main=0x7f030004;
+        public static final int activity_take_photo=0x7f030005;
     }
     public static final class menu {
         public static final int add_claim=0x7f070000;
         public static final int add_item=0x7f070001;
-        public static final int approver_claim_view=0x7f070002;
-        public static final int main=0x7f070003;
-        public static final int take_photo=0x7f070004;
+        public static final int approver_claim_list=0x7f070002;
+        public static final int approver_claim_view=0x7f070003;
+        public static final int main=0x7f070004;
+        public static final int take_photo=0x7f070005;
     }
     public static final class string {
         public static final int action_settings=0x7f050002;
@@ -47,6 +49,7 @@ public final class R {
         public static final int hello_world=0x7f050001;
         public static final int title_activity_add_claim=0x7f050005;
         public static final int title_activity_add_item=0x7f050004;
+        public static final int title_activity_approver_claim_list=0x7f050007;
         public static final int title_activity_approver_claim_view=0x7f050003;
         public static final int title_activity_take_photo=0x7f050006;
     }
--- Team16APP/src/com/example/team16app/Claim.java
+++ Team16APP/src/com/example/team16app/Claim.java
@@ -1,6 +1,86 @@
 package com.example.team16app;
 import java.util.ArrayList;
 
+import android.content.ClipData.Item;
+
 public class Claim {
+
+	public static final String getStatus = null;
+	public String getDestination;
+	public String getName;
+
+	public Claim(Item item) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void getStartDate() {
+		// TODO Auto-generated method stub
+		return;
+	}
+
+	public void getEndDate(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void getStatus(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void getName(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void addDestination(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+	public void getCost(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	public String getDestination() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void getCurrency(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public String getCurrency() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean contains(Item expense) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void addComment(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public String getComment() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+
+	public void getApprover(String string) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
