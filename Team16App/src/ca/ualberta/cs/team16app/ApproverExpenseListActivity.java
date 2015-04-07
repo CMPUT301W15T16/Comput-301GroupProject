5d6f2938b64541b78982a336a32762f94b9ba4e4
leonsariel <leonsariel@gmail.com>
2015-04-06 20:25:50 -0600

    now can display submitted expense list with details

--- Team16App/src/ca/ualberta/cs/team16app/ApproverExpenseListActivity.java
+++ Team16App/src/ca/ualberta/cs/team16app/ApproverExpenseListActivity.java
@@ -4,15 +4,20 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 import android.os.Bundle;
+import android.provider.Settings.Global;
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.DialogInterface.OnClickListener;
 import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemLongClickListener;
 
 public class ApproverExpenseListActivity extends Activity {
 
@@ -20,11 +25,9 @@ public class ApproverExpenseListActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_approver_expense_list);
+		
 		Intent intent = getIntent();
 		Claim claim = (Claim) intent.getSerializableExtra("claim");
-		
-		
-		
 		
 		Collection<Expense> expense = claim.getExpense();
 		
@@ -33,19 +36,54 @@ public class ApproverExpenseListActivity extends Activity {
 		final ArrayAdapter<Expense> expenseAdapter = new ArrayAdapter<Expense>(this, android.R.layout.simple_list_item_1, list);
 	     
 		listView.setAdapter(expenseAdapter);
+		
+		
+		listView.setOnItemClickListener(new OnItemClickListener(){
 
+			@SuppressWarnings("deprecation")
+			@Override
+			public void onItemClick(AdapterView<?> AdapterView, View view,
+					int position, long id) {
+				final int index = position;
+				AlertDialog.Builder adb = new AlertDialog.Builder(
+						ApproverExpenseListActivity.this);
+				adb.setMessage("Expense: "
+						+ list.get(index).getName().toString()
+						+ "\nCategory: "
+						+ list.get(index).getCategory().toString()
+						//+ "\nDate: "
+						//+ list.get(index).getDate().toString()
+						+ "\nAmount: "
+						+ list.get(index).getSpend().toString()
+						+ "\nCurrency Type: "
+						+ list.get(index).getCurrency().toString()
+						+ "\nDescription: "
+						+ list.get(index).getDescription().toString()
+
+				);
+				adb.setPositiveButton("View Photo", new DialogInterface.OnClickListener() {
+			           public void onClick(DialogInterface dialog, int id) {
+			        	   Intent intent = new Intent(ApproverExpenseListActivity.this,
+									ViewReceiptActivity.class);
+							startActivity(intent);
+			           }
+				});
+				adb.show();
+			//public void onItemClick(AdapterView<?> adapterView, View view, int position,
+					//long id)
+					
+				// TODO Auto-generated method stub
+				//Intent intent = new Intent(ExpenseListActivity.this,AddExpenseActivity.class);
+				//startActivity(intent);
+				}
+			});
 		
 		
 		
 		
+							
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.approver_expense_list, menu);
-		return true;
-	}
 	
 	public void backToClaimList(View v){
 		Toast.makeText(this,"back to the Claim list", Toast.LENGTH_SHORT).show(); // show message
@@ -53,5 +91,5 @@ public class ApproverExpenseListActivity extends Activity {
 		Intent intent = new Intent(ApproverExpenseListActivity.this,ApproverClaimListActivity.class);
 		startActivity(intent);
 		}
-
 }
+	
